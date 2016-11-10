7c890432f6a7a4665771321e11be48015cab72bf
bernardolins <bernardolins28@gmail.com>
2016-11-09 23:17:00 -0200

    Fix parameter parsing

--- src/tail.c
+++ src/tail.c
@@ -5,15 +5,20 @@ int main(int argc, char* argv[]) {
 
   const char* file_name = argv[argc-1];
 
-  while((opt = getopt(argc, argv, "nf:")) != -1) {
-    switch(opt) {
-      case 'n':
-       num_of_lines = opt;
-       break;
+  while(optind < argc) {
+    if((opt = getopt(argc, argv, "fn:")) != -1) {
+      switch(opt) {
+        case 'n':
+         num_of_lines = atoi(optarg);
+         break;
 
-      case 'f':
-       follow = 2;
-       break;
+        case 'f':
+         follow = 1;
+         break;
+      }
+    } else {
+      file_name = argv[optind];
+      optind++;
     }
   }
 
