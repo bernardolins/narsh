f49fccc73a163eb47ec86dac8d91cb60fbfbae73
bernardolins <bernardolins28@gmail.com>
2016-11-22 00:43:07 -0200

    Remove unnecessary headers from tail.h

--- inc/tail.h
+++ inc/tail.h
@@ -1,12 +1,3 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <string.h>
 #include "narsh.h"
 
 int num_of_lines = 10;
